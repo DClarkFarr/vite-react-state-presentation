29b204852e728c63db0fb060554349834dba8ae4
Daniel Farr <dfarr@logitech.com>
2022-07-24 23:31:42 -0600

    implement main layout

--- web/src/components/Layout/MainLayout.tsx
+++ web/src/components/Layout/MainLayout.tsx
@@ -1,35 +1,45 @@
 import { ReactNode } from "react";
 import { Outlet } from "react-router-dom";
+import useTasksQuery from "../../hooks/useTasksQuery";
+import useUserQuery from "../../hooks/useUserQuery";
 import CustomLink from "./NavLink";
 
-const MainLayout = ({children = null}: {children?: ReactNode}) => {
+const MainLayout = ({ children = null }: { children?: ReactNode }) => {
+  const { user } = useUserQuery();
+  const { tasks } = useTasksQuery();
+
+  const { tasks: userTasks } = useTasksQuery({ isGeneric: false, userId: user?.id });
 
   return (
     <>
       <div className="header">
         <CustomLink to="/">Home</CustomLink>
         <CustomLink to="/tasks">
+          <span>Tasks</span>
           <span>
-            Tasks
-          </span> 
-          <span>
-            <span className="badge">0</span>
+            <span className="badge">
+              {tasks.length}
+            </span>
           </span>
         </CustomLink>
         <CustomLink to="/profile">
           <span>Profile</span>
           <span>
-            <span className="badge">0</span>
+            <span className="badge">
+              {userTasks.length}
+            </span>
           </span>
         </CustomLink>
 
         <div className="header__right">
-          Welcome, <span className="badge">User?</span>
+          {user && (
+            <>
+              Welcome, <span className="badge">{user.name}</span>
+            </>
+          )}
         </div>
       </div>
-      <div className="main">
-        {children}
-      </div>
+      <div className="main">{children}</div>
       <div className="footer">
         &copy; 2022 - DCF Coders - All rights reserved
       </div>
